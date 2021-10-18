15cd8c3cfccba3c265ee1e20b1bdfb476f448421
Sharad Ranpara <ranpas56@uwosh.edu>
2021-10-17 22:31:58 -0500

    Chat page with Exampl

--- SportConnect/Talk.cs
+++ SportConnect/Talk.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static SportConnect.Talk;
 
 namespace SportConnect
 {
@@ -11,19 +12,16 @@ namespace SportConnect
     {
         public Talk()
         {
-            this.Add(new Message() { Sender = "Adventure Works", Content = "Hi, what can we do for you?" });
-            this.Add(new Message() { Sender = "Client", Content = "Did you receive the GR268 KZ bike?" });
-            this.Add(new Message() { Sender = "Adventure Works", Content = "Not yet, but we have a similar model available." });
-            this.Add(new Message() { Sender = "Client", Content = "What is it like?" });
-            this.Add(new Message() { Sender = "Adventure Works", Content = "It boasts a carbon frame, hydraulic brakes and suspension, and a gear hub." });
-            this.Add(new Message() { Sender = "Client", Content = "How much does it cost?" });
-            this.Add(new Message() { Sender = "Adventure Works", Content = "Same as the GR268 KZ model you requested. You can get it from our online shop." });
-            this.Add(new Message() { Sender = "Client", Content = "Thanks." });
-            this.Add(new Message() { Sender = "Adventure Works", Content = "Thank you, have a nice ride." });
-        }
-    }
+            // Example of content for the Chat. 
+            this.Add(new Message() { Sender = "Sam", Content = "Hi, did you sign up for the upcoming pool tournament?" });
+            this.Add(new Message() { Sender = "Daniel", Content = "I signed up for the one in July" });
+            this.Add(new Message() { Sender = "Sam", Content = "We are short of one player. Would you like to join?" });
+            this.Add(new Message() { Sender = "Daniel", Content = "Count me in!! I was looking for a team to join" });
+            this.Add(new Message() { Sender = "Sam", Content = "Awesome!! Broskiii we going to win. We got a good team this year" });
 
-    public class Message
+        }
+
+        public class Message
     {
         public string Sender { get; set; }
         public string Content { get; set; }
