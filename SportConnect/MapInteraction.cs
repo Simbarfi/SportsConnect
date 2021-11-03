cc66ab8c64dc6d05a12ebd2c024755eb9e991279
abelt90 <abelt90@uwosh.edu>
2021-11-03 17:46:43 -0500

    Added MapInteraction to handle working with the map

---
+++ SportConnect/MapInteraction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Runtime.InteropServices;
+
+namespace SportConnect
+{
+    [ComVisible(true)]
+    public class MapInteraction
+    {
+        private Window parentWindow;
+        private WebBrowser browser;
+
+        public MapInteraction(WebBrowser wb,
+                              Window win)
+        {
+            parentWindow = win;
+            browser = wb;
+        }
+        public bool CreateEvent(string msg)
+        {
+            AddEventWindow addWin = new AddEventWindow();
+            addWin.Owner = parentWindow;
+            addWin.ShowDialog();
+
+            //try
+            //{
+            //    //browser.InvokeScript("AddEvent");
+            //}
+            //catch (COMException)
+            //{
+
+            //}
+            return true;
+        }
+        public string[] ViewEvent()
+        {
+            MessageBox.Show("Future Event Information Handling");
+
+            return null;
+        }
+
+        public void DisplayEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
