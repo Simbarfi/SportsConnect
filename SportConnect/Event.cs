c1b408d0618f6630ad3644f93aa31bf1827407c0
abelt90 <abelt90@uwosh.edu>
2021-11-07 16:07:43 -0600

    Created a class to house an event

---
+++ SportConnect/Event.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace SportConnect
+{
+    public class Event
+    {
+        public Event() { }
+        public Event(string name,
+                    string sport,
+                    string start,
+                    string end,
+                    int maxPlayers,
+                    string skill, 
+                    string location)
+        {
+            Name = name;
+            Sport = sport;
+            string format = "d";
+            DateTime result;
+            DateTime.TryParseExact(start,
+                                    format,
+                                    CultureInfo.InvariantCulture,
+                                    DateTimeStyles.None,
+                                    out result);
+            Start = result;
+            DateTime.TryParseExact(end,
+                                    format,
+                                    CultureInfo.InvariantCulture,
+                                    DateTimeStyles.None,
+                                    out result);
+            End = result;
+            MaxPlayers = MaxPlayers;
+            SkillLevel = skill;
+            Location = location;
+        }
+        public int Id {get; set;}
+        public string Name { get; set; }
+        public string Sport { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int MaxPlayers { get; set; }
+        public string SkillLevel { get; set; }
+        public string Location { get; set; }
+    }
+}
